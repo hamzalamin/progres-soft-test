10b7be7f6cb8d284627433f75899c3f1f28708d7
hamzalamin <hamzalamin80@gmail.com>
2025-02-20 15:55:54 +0100

    first commit

---
+++ src/main/java/com/progresoft/progresofttest/ProgresoftTestApplication.java
@@ -0,0 +1,13 @@
+package com.progresoft.progresofttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgresoftTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgresoftTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/progresoft/progresofttest/ProgresoftTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progresoft.progresofttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgresoftTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
